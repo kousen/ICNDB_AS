be6dc1f54b51678f5e056b1c32cabbfb27695f53
Kenneth Kousen <ken.kousen@kousenit.com>
2014-06-12 12:13:40 -0700

    added robotium tests for MainActivity

---
+++ app/src/androidTest/java/com/nfjs/icndb/app/MainActivityTest.java
@@ -0,0 +1,30 @@
+package com.nfjs.icndb.app;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private Solo solo;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testMainActivity() {
+        solo.assertCurrentActivity("MainActivity", MainActivity.class);
+    }
+
+    public void testJokeButton() {
+        solo.clickOnButton("Get Joke");
+        assertTrue(solo.searchText("Hans"));
+    }
+
+}
