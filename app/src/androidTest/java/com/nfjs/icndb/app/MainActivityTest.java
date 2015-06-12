b66a45cdb5187bed4bce6dbb38a427312b3bb4b3
Kenneth Kousen <ken.kousen@kousenit.com>
2015-06-12 13:58:59 -0700

    Use espresso for tests intead of robotium

--- app/src/androidTest/java/com/nfjs/icndb/app/MainActivityTest.java
+++ app/src/androidTest/java/com/nfjs/icndb/app/MainActivityTest.java
@@ -1,30 +1,41 @@
 package com.nfjs.icndb.app;
 
-import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
 
-import com.robotium.solo.Solo;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-    private Solo solo;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.endsWith;
 
-    public MainActivityTest() {
-        super(MainActivity.class);
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivity() {
+        onView(allOf(isDisplayed(), withClassName(endsWith("MainActivity"))));
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-
-        solo = new Solo(getInstrumentation(), getActivity());
+    @Test
+    public void jokeButtonUsesHeroName() {
+        onView(withId(R.id.icndb_button))
+                .perform(click());
+        onView(withId(R.id.text_view))
+                .check(matches(withText(containsString("Hans"))));
     }
-
-    public void testMainActivity() {
-        solo.assertCurrentActivity("MainActivity", MainActivity.class);
-    }
-
-    public void testJokeButton() {
-        solo.clickOnButton("Get Joke");
-        assertTrue(solo.searchText("Hans"));
-    }
-
 }
